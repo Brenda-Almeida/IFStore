323f3453cf4172ca00e0493f8a73c9a7677ba26b
Leandro Augusto <leandroaugusto9999@gmail.com>
2021-03-17 19:24:48 -0300

    add Pedido View

---
+++ src/view/PedidoView.java
@@ -0,0 +1,17 @@
+package view;
+
+import java.awt.Button;
+import java.awt.Label;
+import java.util.List;
+
+public class PedidoView {
+	public Label labelIdPedido;
+	public List<ProdutoView> listProdutoPedido;
+	public Label labelSituacaoPedido;
+	public Button btnEmitirBoleto;
+	
+	public void botaoEmitirBoletoClick() {
+		
+	}
+	
+}
