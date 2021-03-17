f9d311d5b4183e7fdecaa87a14a8c0a9ba662614
Leandro Augusto <leandroaugusto9999@gmail.com>
2021-03-17 19:24:06 -0300

    add LoginView

---
+++ src/view/LoginView.java
@@ -0,0 +1,22 @@
+package view;
+
+import java.awt.Button;
+import java.awt.Label;
+
+public class LoginView {
+	public Label labelEmail;
+	public InputText inputEmail;
+	public Label labelSenha;
+	public InputText inputSenha;
+	public Button botaoLogin;
+	public Button botaoCadastrar;
+
+	public void botaoLoginClick() {
+
+	}
+
+	public void botaoCadastraClick() {
+
+	}
+
+}
