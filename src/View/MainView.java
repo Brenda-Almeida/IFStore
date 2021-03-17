84d054add45e2d20f674a7783442bea9f2d04b1e
Brenda Almeida <brendabdg000@gmail.com>
2021-03-17 14:33:47 -0300

    Adicionando MainView

---
+++ src/View/MainView.java
@@ -0,0 +1,21 @@
+package View;
+
+import java.awt.Button;
+import java.awt.Label;
+import java.awt.TextField;
+
+public class MainView {
+	public Label labelCabecalho;
+	public Label labelPesquisa;
+	public TextField inputPesquisa;
+	public Button btnPesquisar;
+	public Label labelProdutos;
+	public ProdutoView listProduto;
+
+	public void botaoPesquisarClick() {
+	};
+
+	public void imagemClick() {
+	};
+
+}
