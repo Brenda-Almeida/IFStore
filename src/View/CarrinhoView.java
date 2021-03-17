51fb1e1c3a84441447aab6af69e2f185ab66afff
Brenda Almeida <brendabdg000@gmail.com>
2021-03-17 14:26:36 -0300

    adicionando classes da View

---
+++ src/View/CarrinhoView.java
@@ -0,0 +1,19 @@
+package View;
+
+import java.awt.Button;
+import java.awt.Label;
+import java.awt.TextField;
+
+public class CarrinhoView {
+	public ProdutoView listProduto;
+	public Label labelQuantidade;
+	public TextField inputQuantidade;
+	public Button btnComprar;
+	public Button btnRemoverProduto;
+
+	public void botaoComprarClick() {
+	}
+
+	public void botaoRemoverProdutoClick() {
+	}
+}
