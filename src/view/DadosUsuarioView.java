b3005ab86f61d6dd084ddc12cc6e436ad67757c9
Leandro Augusto <leandroaugusto9999@gmail.com>
2021-03-17 19:23:11 -0300

    add DadosUsuarioView

---
+++ src/view/DadosUsuarioView.java
@@ -0,0 +1,35 @@
+package view;
+
+import java.awt.Button;
+import java.awt.Label;
+
+public class DadosUsuarioView {
+	public Label labelNome;
+	public Input inputNome;
+	public Label labelEmail;
+	public Input inputEmail;
+	public Label labelSenha;
+	public Input inputSenha;
+	public Label labelTelefone;
+	public Input inputTelefone;
+	public Label labelEndereco;
+	public Input inputEndereco;
+	public Label labelCpf;
+	public Input inputCpf;
+	public Button btnSalvar;
+	public Button btnEditar;
+	public Button btnExcluir;
+
+	public void botaoSalvarClick() {
+
+	}
+
+	public void botaoEditarClick() {
+
+	}
+
+	public void botaoExcluirClick() {
+
+	}
+
+}
