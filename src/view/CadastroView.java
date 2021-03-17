7ed4392e07de8e73d01905da8e1e78f35ffcaf7b
Leandro Augusto <leandroaugusto9999@gmail.com>
2021-03-17 19:20:21 -0300

    add CadastroView

---
+++ src/view/CadastroView.java
@@ -0,0 +1,24 @@
+package view;
+
+import java.awt.Label;
+
+public class CadastroView {
+	public Label labelNome;
+	public Input inputNome;
+	public Label labelEmail;
+	public Input inputEmail;
+	public Label labelSenha;
+	public Input inputSenha;
+	public Label labelTelefone;
+	public Input inputTelefone;
+	public Label labelEndereco;
+	public Input inputEndereco;
+	public Label labelCpf;
+	public Input inputCpf;
+	public Button btnSalvar;
+
+	public void botaoSalvarClick() {
+
+	}
+
+}
